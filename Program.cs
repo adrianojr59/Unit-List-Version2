880c0a4b660e05b490d19ea0e5741f05a040bba4
adria <adria@ADRIANO>
2022-01-14 08:16:07 -0300

    adicionado opção para remover cadastros  e outros tipos de alteração agora e possivel pela conversao do tipo List tradicional

--- Program.cs
+++ Program.cs
@@ -18,20 +18,23 @@ person.AddHashList(new Person("Bob", "323284732432843"));
 SortedSet<Person>ListAll = new SortedSet<Person>(person.ListHash);ListAll.UnionWith(_Bussines.ListHash);
 
 
-var people = ListAll.OrderBy(c => c.CpfeCnpj); // ordernar por cpf e cnpj ou pelo nome 
+//var people = ListAll.OrderBy(c => c.CpfeCnpj); // ordernar por cpf e cnpj ou pelo nome 
 
 
 
-ListAll.ToList().ForEach(c => c.CpfeCnpj += 22); // adicionar em todos cadastro 22 ao fim  de cada numero ideal para colocar promoção etc
+//ListAll.ToList().ForEach(c => c.CpfeCnpj += 22); // adicionar em todos cadastro 22 ao fim  de cada numero ideal para colocar promoção etc
+
+//List<Person> people = new List<Person>(ListAll.ToList());   // para alterar remover ou fazer qualquer coisa em uma lista precisa sair do tipo baixo nivel e ir para uma lista
+//people.RemoveAll(p => p.Name.Length > 2);
 
 
 
 
 ListView(ListAll);
 
-static void ListView <T>(IEnumerable<T> list)
+static void ListView <T>(IEnumerable<T> view)
 {
-    foreach (T item in list)
+    foreach (T item in view)
     {
         Console.WriteLine(item);
     }
